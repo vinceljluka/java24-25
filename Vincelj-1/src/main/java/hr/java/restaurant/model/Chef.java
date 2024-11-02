6add2c45e77ab2cb6a6934f0ab24ff4f0a4e2562
Luka Vincelj <lukavinc9@gmail.com>
2024-11-02 11:27:08 +0100

    Update Chef.java

--- Vincelj-1/src/main/java/hr/java/restaurant/model/Chef.java
+++ Vincelj-1/src/main/java/hr/java/restaurant/model/Chef.java
@@ -1,6 +1,7 @@
 package hr.java.restaurant.model;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Chef {
     private String firstName;
@@ -36,4 +37,32 @@ public class Chef {
     public void setSalary(BigDecimal salary) {
         this.salary = salary;
     }
+
+    public static void inputChef(Chef[] chefs, Scanner scanner) {
+        for (int i = 0; i < chefs.length; i++) {
+            System.out.print("Enter " + (i+1) + "." +  " chef's first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("Enter " + (i+1) + "." +  " chef's last name: ");
+            String lastName = scanner.nextLine();
+
+            BigDecimal salary;
+            while (true) {
+                System.out.print("Enter " + (i+1) + "." + " chef's salary: ");
+                if (scanner.hasNextBigDecimal()) {
+                    salary = scanner.nextBigDecimal();
+                    scanner.nextLine();
+                    if (salary.compareTo(BigDecimal.ZERO) >= 0) {
+                        break;
+                    } else {
+                        System.out.println("Salary cannot be negative. Please enter a valid salary.");
+                    }
+                } else {
+                    System.out.println("Invalid input, please enter a valid salary.");
+                    scanner.nextLine();
+                }
+            }
+            chefs[i] = new Chef(firstName, lastName, salary);
+        }
+    }
+
 }
