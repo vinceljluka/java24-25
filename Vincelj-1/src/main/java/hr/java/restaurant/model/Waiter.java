492333f77eba2eff462adea4f2f7b20deca90b6e
Luka Vincelj <lukavinc9@gmail.com>
2024-11-02 11:31:12 +0100

    Update Waiter.java

--- Vincelj-1/src/main/java/hr/java/restaurant/model/Waiter.java
+++ Vincelj-1/src/main/java/hr/java/restaurant/model/Waiter.java
@@ -1,6 +1,7 @@
 package hr.java.restaurant.model;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Waiter {
     private String firstName;
@@ -36,4 +37,36 @@ public class Waiter {
     public void setSalary(BigDecimal salary) {
         this.salary = salary;
     }
+
+    public static void inputWaiter (Waiter[] waiters, Scanner scanner) {
+        for (int i = 0; i < waiters.length; i++)
+        {
+            System.out.print("Enter " + (i+1) + "." +  " waiter's first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("Enter " + (i+1) + "." +  " waiter's last name: ");
+            String lastName = scanner.nextLine();
+
+            BigDecimal salary;
+            while (true) {
+                System.out.print("Enter " + (i+1) + "." + " waiters's salary: ");
+                if (scanner.hasNextBigDecimal()) {
+                    salary = scanner.nextBigDecimal();
+                    scanner.nextLine();
+                    if (salary.compareTo(BigDecimal.ZERO) >= 0) {
+                        break;
+                    } else {
+                        System.out.println("Salary cannot be negative. Please enter a valid salary.");
+                    }
+                } else {
+                    System.out.println("Invalid input, please enter a valid salary.");
+                    scanner.nextLine();
+                }
+            }
+            waiters[i] = new Waiter(firstName, lastName, salary);
+        }
+    }
+
 }
+
+
+
