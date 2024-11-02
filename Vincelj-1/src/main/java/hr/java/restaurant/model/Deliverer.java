c262649abfeceea91897f2edae6a3e379a6fdabe
Luka Vincelj <lukavinc9@gmail.com>
2024-11-02 11:28:46 +0100

    Update Deliverer.java

--- Vincelj-1/src/main/java/hr/java/restaurant/model/Deliverer.java
+++ Vincelj-1/src/main/java/hr/java/restaurant/model/Deliverer.java
@@ -1,6 +1,7 @@
 package hr.java.restaurant.model;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Deliverer {
     private String firstName;
@@ -35,5 +36,33 @@ public class Deliverer {
 
     public void setSalary(BigDecimal salary) {
         this.salary = salary;
+    }
+
+    public static void inputDeliverer (Deliverer[] deliverers, Scanner scanner ) {
+        for (int i = 0; i < deliverers.length; i++)
+        {
+            System.out.print("Enter " + (i+1) + "." +  " deliverers first name: ");
+            String firstName = scanner.nextLine();
+            System.out.print("Enter " + (i+1) + "." +  " deliverers last name: ");
+            String lastName = scanner.nextLine();
+
+            BigDecimal salary;
+            while (true) {
+                System.out.print("Enter " + (i+1) + "." + " deliverer's salary: ");
+                if (scanner.hasNextBigDecimal()) {
+                    salary = scanner.nextBigDecimal();
+                    scanner.nextLine();
+                    if (salary.compareTo(BigDecimal.ZERO) >= 0) {
+                        break;
+                    } else {
+                        System.out.println("Salary cannot be negative. Please enter a valid salary.");
+                    }
+                } else {
+                    System.out.println("Invalid input, please enter a valid salary.");
+                    scanner.nextLine();
+                }
+            }
+            deliverers[i] = new Deliverer(firstName, lastName, salary);
+        }
     }
 }
